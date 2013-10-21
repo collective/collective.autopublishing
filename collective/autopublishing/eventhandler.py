d94e7e4611a9142d3fb5808813c516945063192e
Sune Broendum Woeller <sune@woeller.dk>
2013-10-21 19:32:29 +0200

    Add the actual retraction part + tests.

--- collective/autopublishing/eventhandler.py
+++ collective/autopublishing/eventhandler.py
@@ -17,11 +17,7 @@ logger = logging.getLogger('collective.autopublishing')
 # one of the tick events from collective.timedevents
 
 def AutoPublishHandler(event):
-    '''
-    '''
-    context = event.context
-    catalog = context.portal_catalog
-    wf = context.portal_workflow
+    catalog = event.context.portal_catalog
 
     try:
         settings = getUtility(IRegistry).forInterface(IAutopublishSettingsSchema)
@@ -35,12 +31,21 @@ def AutoPublishHandler(event):
         logger.info('Catalog does not have a enableAutopublishing index')
         return
 
+    handle_publishing(event, settings, has_field)
+    handle_retracting(event, settings, has_field)
+
+def handle_publishing(event, settings, has_field):
+    '''
+    '''
+    catalog = event.context.portal_catalog
+    wf = event.context.portal_workflow
+
     states_to_publish = settings.initial_states_publishing
     if not states_to_publish:
         logger.info('You have to define state-to-publish in the plone control panel')
         return
 
-    now = context.ZopeTime()
+    now = event.context.ZopeTime()
     query = (In('review_state', states_to_publish)
              & Eq('effectiveRange', now))
 
@@ -73,8 +78,6 @@ def AutoPublishHandler(event):
             total += 1
             if not settings.dry_run:
                 try:
-                    # this is a naive approach - and will throw an exception if the state
-                    # don't provide the publish workflow action.
                     if has_field:
                         o.setEnableAutopublishing(False)
                     wf.doActionFor(o, 'publish')
@@ -85,5 +88,56 @@ def AutoPublishHandler(event):
                                    object at '%s' does not provide the publish action
                                 """ % (brain.review_state, o.getURL()))
 
-    logger.info("""Ran collective.autopublishing. %d objects found, %d affected
+    logger.info("""Ran collective.autopublishing publish: %d objects found, %d affected
+                """ % (total, affected))
+
+def handle_retracting(event, settings, has_field):
+    '''
+    '''
+    catalog = event.context.portal_catalog
+    wf = event.context.portal_workflow
+
+    states_to_retract = settings.initial_states_retracting
+    if not states_to_retract:
+        logger.info('You have to define state-to-retract in the plone control panel')
+        return
+
+    now = event.context.ZopeTime()
+    query = (In('review_state', states_to_retract)
+             & Le('expires', now))
+
+    if has_field:
+        query = query & Eq('enableAutopublishing', True)
+
+    brains = catalog.evalAdvancedQuery(query)
+
+    affected = 0
+    total = 0
+    for brain in brains:
+        logger.info('Found %s' % brain.getURL())
+        o = brain.getObject()
+        eff_date = o.getEffectiveDate()
+        exp_date = o.getExpirationDate()
+        # The dates in the indexes are always set.
+        # So we need to test on the objects if the dates
+        # are set.
+
+        # we only retract if:
+        # the expiration date is set and is in the past:
+        if exp_date is not None and exp_date < now:
+            logger.info('Retracting %s' % brain.getURL())
+            total += 1
+            if not settings.dry_run:
+                try:
+                    if has_field:
+                        o.setEnableAutopublishing(False)
+                    wf.doActionFor(o, 'retract')
+                    o.reindexObject()
+                    affected += 1
+                except WorkflowException:
+                    logger.info("""The state '%s' of the workflow associated with the
+                                   object at '%s' does not provide the retract action
+                                """ % (brain.review_state, o.getURL()))
+
+    logger.info("""Ran collective.autopublishing retract: %d objects found, %d affected
                 """ % (total, affected))
