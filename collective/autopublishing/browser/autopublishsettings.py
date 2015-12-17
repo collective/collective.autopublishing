6f94c525a0de847b446a5430faacc9c8e48c93b6
Tom Gross <itconsense@gmail.com>
2015-12-17 12:45:13 +0000

    added german translation file und updated translations
    use labels for translations
    added german translations

--- collective/autopublishing/browser/autopublishsettings.py
+++ collective/autopublishing/browser/autopublishsettings.py
@@ -23,27 +23,31 @@ logger = logging.getLogger('collective.autopublishing')
 class IAutopublishSpecification(Interface):
     portal_types = schema.Tuple(
         title=_(u"Content types"),
-        description=_(u"Content types this rule applies to."),
+        description=_('help_ap_portaltypes',
+                      default=u"Content types this rule applies to."),
         required=True,
         missing_value=(),
         value_type=schema.Choice(
             vocabulary="plone.app.vocabularies.ReallyUserFriendlyTypes"))
     initial_state = schema.TextLine(
-        title=_(u"Initial workflow state"),
+        title=_('label_initial_state', default=u"Initial workflow state"),
         required=True,
         )
     transition = schema.TextLine(
-        title=_(u"Transition from initial state"),
+        title=_('label_transition', default=u"Transition from initial state"),
         required=True,
         )
     date_index = schema.TextLine(
-        title=_(u"Transaction trigger date catalog index / method"),
-        description=_(u"By default publishing date (the 'effective' index) is "
-                      u"used for 'publish actions', and expiration date (the "
-                      u"'expires' index) is used for 'retract actions'."
-                      u" If a custom method is to be used for the date, enter"
-                      u" index_id|object_method_id. If index_id = method_id "
-                      u" just enter index_id."),
+        title=_('label_date_index',
+                default=u"Transaction trigger date catalog index / method"),
+        description=_(
+            'help_date_index',
+            default=u"By default publishing date (the 'effective' index) is "
+                    u"used for 'publish actions', and expiration date (the "
+                    u"'expires' index) is used for 'retract actions'."
+                    u" If a custom method is to be used for the date, enter"
+                    u" index_id|object_method_id. If index_id = method_id "
+                    u" just enter index_id."),
         required=False,
         default=u'effective',
         missing_value=u'effective'
@@ -73,9 +77,10 @@ class IAutopublishSettingsSchema(Interface):
         value_type=schema.Object(
             title=_(u'Action'),
             schema=IAutopublishSpecification),
-        title=_(u'Publish actions'),
-        description=_(u"Workflow actions initiated by the autopublishing "
-                      u"module when the publishing date is met."),
+        title=_('label_publish_actions', default=u'Publish actions'),
+        description=_('help_publish_actions',
+            default=u"Workflow actions initiated by the autopublishing "
+                    u"module when the publishing date is met."),
         required=False,
         default=(),
         missing_value=())
@@ -83,40 +88,47 @@ class IAutopublishSettingsSchema(Interface):
         value_type=schema.Object(
             title=_(u'Action'),
             schema=IAutopublishSpecification),
-        title=_(u'Retract actions'),
-        description=_(u"Workflow actions initiated by the autopublishing "
-                      u"module when the expiration date is met."),
+        title=_('label_retract_actions', default=u'Retract actions'),
+        description=_('help_retract_actions',
+            default=u"Workflow actions initiated by the autopublishing "
+                    u"module when the expiration date is met."),
         required=False,
         default=(),
         missing_value=())
     overwrite_expiration_on_retract = schema.Bool(
-        title=_(u'Set expiration date on retraction'),
-        description=_(u"If this is set, the expiration date "
-                      u"will be overwritten with the current time "
-                      u"when manually retracting an item, to avoid "
-                      u" republication if there is a publication date "
-                      u"in the past."),
+        title=_('label_overwrite_expiration',
+                default=u'Set expiration date on retraction'),
+        description=_('help_overwrite_expiration',
+            default=u"If this is set, the expiration date "
+                    u"will be overwritten with the current time "
+                    u"when manually retracting an item, to avoid "
+                    u" republication if there is a publication date "
+                    u"in the past."),
         default=False)
     clear_expiration_on_publish = schema.Bool(
-        title=_(u'Clear expiration date on publication'),
-        description=_(u"If this is set, an expiration date "
-                      u"in the past will be cleared "
-                      u"when manually publishing an item, to avoid "
-                      u"immediate retraction."),
+        title=_('label_clear_expiration',
+                default=u'Clear expiration date on publication'),
+        description=_('help_clear_expiration',
+                default=u"If this is set, an expiration date "
+                        u"in the past will be cleared "
+                        u"when manually publishing an item, to avoid "
+                        u"immediate retraction."),
         default=False)
     email_log = schema.List(
-        value_type=schema.TextLine(
-            title=u'Email'),
-        title=_(u'Email addresses for audit log'),
-        description=_(u"If one or more email addresses is supplied, "
-                      u"an audit log of the actions taken is sent."),
+        value_type=schema.TextLine(title=u'Email'),
+        title=_('label_email_log',
+                default=u'Email addresses for audit log'),
+        description=_('help_email_log',
+            default=u"If one or more email addresses is supplied, "
+                    u"an audit log of the actions taken is sent."),
         required=False)
     dry_run = schema.Bool(
-        title=_(u'Simulate'),
-        description=_(u"Simulates the process of changing workflow state. "
-                      u"Nothing actually changes state, but it is possible to "
-                      u"review what actions will be taken. To activate "
-                      u"the module, remove the checkmark."),
+        title=_('label_dry_run', default=u'Simulate'),
+        description=_('help_dry_run',
+            default=u"Simulates the process of changing workflow state. "
+                    u"Nothing actually changes state, but it is possible to "
+                    u"review what actions will be taken. To activate "
+                    u"the module, remove the checkmark."),
         default=True)
 
 
@@ -124,8 +136,8 @@ class AutopublishControlPanelEditForm(controlpanel.RegistryEditForm):
     schema = IAutopublishSettingsSchema
     fields = field.Fields(IAutopublishSettingsSchema)
 
-    label = _(u"Autopublishing")
-    description = _(
+    label = _('label_ap_contolpanel', default=u"Autopublishing settings")
+    description = _('help_ap_controlpanel',
         u"Controls the workflow actions autopublishing will make."
         )
 
